b19cdfbf318bd1d5d52bb213f6086430fc015aae
Kunal-StArK <kunal73014@gmail.com>
2026-07-09 12:36:11 +0530

    create api_view by classBased Views

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('students/',views.studentView ),
     path('students/<int:pk>',views.studentDetailsView),
 
-    # path('employee/',views.Employees.as_view()),
-    # path('employee/<int:pk>',views.EmployeesDetails.as_view())
+    path('employee/',views.Employees.as_view()),
+    path('employee/<int:pk>',views.EmployeesDetails.as_view())
     
 ]
