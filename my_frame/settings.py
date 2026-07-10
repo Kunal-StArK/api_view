31542dd01e09f051c0e4c5054ef302c02d723371
Kunal-StArK <kunal73014@gmail.com>
2026-07-10 22:55:51 +0530

    apply filter but don't success

--- my_frame/settings.py
+++ my_frame/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'student',
     'api',
     'employee',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
